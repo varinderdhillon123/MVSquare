f092be70ec7575967f981053521b260508503e46
varinderdhillon123 <varinder701dhillon@gmail.com>
2020-01-13 19:56:23 -0500

    commit

--- MVSquare/ViewController.swift
+++ MVSquare/ViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCount: UILabel!
     @IBOutlet weak var myView: UIView!
       var timer: Timer!
-    
+     var c = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       lblCount.text = "\(c)"
           let mainViewHeight = view.frame.size.height
             let mainViewWidth = view.frame.size.width
             var myViewHeight = self.myView.frame.size.height
             var myViewWidth = self.myView.frame.size.width
             myView.center = view.center
             myView.backgroundColor = .red
+        
+        var timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+             { timer in
+                self.myView.backgroundColor = .random()
+             }
             expand()
             }
          
@@ -43,7 +50,13 @@ class ViewController: UIViewController {
             }
             
           }
-          func shrink(){
+    func shrink(){
+        
+    self.c+=1
+         lblCount.text = "\(c)"
+    
+          
+          
            var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
            { timer in
              self.myView.backgroundColor = .random()
@@ -60,6 +73,7 @@ class ViewController: UIViewController {
           }
           }
         }
+
         extension UIColor {
           static func random() -> UIColor {
             func random() -> CGFloat { return .random(in:0...1) }
