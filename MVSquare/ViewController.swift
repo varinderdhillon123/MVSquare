507da9ff3ba18a0127cad682a4fc85967449fd3c
varinderdhillon123 <varinder701dhillon@gmail.com>
2020-01-13 19:31:21 -0500

    Initial Commit

---
+++ MVSquare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVSquare
+//
+//  Created by MacStudent on 2020-01-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
