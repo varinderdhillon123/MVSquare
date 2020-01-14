08d50c15e8cc7a14630af61aed78d52fd1283732
varinderdhillon123 <varinder701dhillon@gmail.com>
2020-01-13 19:43:20 -0500

    commit

--- MVSquare/ViewController.swift
+++ MVSquare/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
+      var timer: Timer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+          let mainViewHeight = view.frame.size.height
+            let mainViewWidth = view.frame.size.width
+            var myViewHeight = self.myView.frame.size.height
+            var myViewWidth = self.myView.frame.size.width
+            myView.center = view.center
+            myView.backgroundColor = .red
+            expand()
+            }
+         
+           
+          func expand(){
+            var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+            { timer in
+                  self.myView.backgroundColor = .random()
+                       print("Expand by\(timer)")
+            self.myView.frame.origin.x -= 5
+            self.myView.frame.origin.y -= 5
+            self.myView.frame.size.height += 10
+            self.myView.frame.size.width += 10
+            if(self.myView.frame.size.width == 380)
+            {
+              timer.invalidate()
+              self.shrink()
+            }
+            }
+            
+          }
+          func shrink(){
+           var timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true)
+           { timer in
+             self.myView.backgroundColor = .random()
+                   print("Expand by\(timer)")
+            self.myView.frame.origin.x += 5
+            self.myView.frame.origin.y += 5
+            self.myView.frame.size.height -= 10
+            self.myView.frame.size.width -= 10
+            if(self.myView.frame.size.width == 50)
+            {
+               timer.invalidate()
+              self.expand()
+            }
+          }
+          }
+        }
+        extension UIColor {
+          static func random() -> UIColor {
+            func random() -> CGFloat { return .random(in:0...1) }
+            return UIColor(red:  random(),
+                    green: random(),
+                    blue: random(),
+                    alpha: 1.0)
+          }
+        }
